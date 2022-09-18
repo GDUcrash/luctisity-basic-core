1a3cebbadc142fc116cf6219d3c40152a29cee59
Ucrash <gducrash@gmail.com>
2022-09-18 17:17:24 +0300

    add backdrop node

--- src/objects/backdrop.ts
+++ src/objects/backdrop.ts
@@ -1,9 +1,16 @@
+import FrameCollectionComponent from "../objectComponenets/frameCollectionComponent";
 import CoreNode from "./coreNode";
 
 export default class Backdrop extends CoreNode {
 
     constructor (name: string, id?: string) {
         super(name, id);
+
+        this.components.add(new FrameCollectionComponent(this));
+    }
+
+    getType () {
+        return Backdrop;
     }
 
 }
